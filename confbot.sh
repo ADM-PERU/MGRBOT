39553f9d23e0bf9485f9bdd5ace00e54eac8f273
ADM-PERU <90083965+ADM-PERU@users.noreply.github.com>
2022-01-14 13:19:07 -0500

    Update confbot.sh

--- confbot.sh
+++ confbot.sh
@@ -21,7 +21,7 @@ function_verify () {
   [[ $(echo $permited|grep "${IP}") = "" ]] && {
   clear
   echo -e "\n\n\n\e[31m====================================================="
-  echo -e "\e[31m      ¡LA IP $(wget -qO- ipv4.icanhazip.com) NO ESTA AUTORIZADO!\n     SI DESEAS USAR EL BOT-GEN CONTACTE A @GENKEY_BOT VIA TELEGRAM."
+  echo -e "\e[31m         ¡LA IP $(wget -qO- ipv4.icanhazip.com) NO ESTA AUTORIZADO!\n     SI DESEAS USAR EL BOT-GEN CONTACTE A @GENKEY_BOT\n                       VIA TELEGRAM."
   echo -e "\e[31m=====================================================\n\n\n\e[0m"
   [[ -d /etc/ADM-db ]] && rm -rf /etc/ADM-db
 [[ ! -e "/bin/ShellBot.sh" ]] && rm /bin/ShellBot.sh
